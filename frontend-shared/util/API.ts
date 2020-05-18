f44dd924e99394448740506ceaccfa9d280898e1
Christiaan Kruger <c@hristiaan.co.za>
2020-05-18 22:34:16 +0200

    Prevent double sends on Post level

--- frontend-shared/util/API.ts
+++ frontend-shared/util/API.ts
@@ -2,10 +2,24 @@ import Axios from 'axios';
 
 const ROOT = '';
 
+let lastSent: { url: string; body: object } = undefined;
+
 /**
  * e.g. Post('/join');
  */
 export const Post = async <T>(url: string, body: object) => {
+  /**
+   * KLUDGE: Some UI elements respond to double taps.
+   * It should be fixed and prevented, but for now this is easiest.
+   * As of writing (May 2020), there exists no valid reason to send the exact same command twice in a row.
+   */
+  if (matchLastSent(url, body)) {
+    // Probably a double send, ignore.
+    return;
+  }
+
+  lastSent = { url, body };
+
   console.log(`[Post] '${url}' with body: ${JSON.stringify(body)}`);
   try {
     const result = await Axios.post(`${ROOT}${url}`, body);
@@ -16,4 +30,11 @@ export const Post = async <T>(url: string, body: object) => {
     }
     throw new Error(error);
   }
+};
+
+const matchLastSent = (url: string, body: object): boolean => {
+  if (!lastSent) {
+    return false;
+  }
+  return url === lastSent.url && JSON.stringify(body) === JSON.stringify(lastSent.body);
 };
